96b6fd1c54c419daa5b756add34fb48ccf25502a
qinxiaobo <1337556808@qq.com>
2024-10-09 22:43:54 +0800

    nodepay项目接口调用

--- services/chromes/tasks/nodepay.py
+++ services/chromes/tasks/nodepay.py
@@ -1,7 +1,6 @@
 import asyncio
 import datetime
 import time
-
 from flaskServer.services.internal.tls.client import TLSClient
 from flaskServer.entity.galxeAccount import AccountInfo
 from flaskServer.mode.env import Env
@@ -10,8 +9,9 @@ from flaskServer.mode.account import Account
 from flaskServer.config.connect import app
 
 token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjkzMjE1MzQ2NjUxNTYxOTg0IiwiaWF0IjoxNzI4NDc3NDQ1LCJleHAiOjE3Mjk2ODcwNDV9.PVA7RDdsTH_8UflzBMIQhHRPBwP229C6bxSG9FL78KhniY6d8iUvpg5ywIOuhH43gO8GIHOP14NwUkqsiWE7Kw"
-
-async def toKeep(token,proxy,env_name,username):
+browser_id = "e1998100-a629-4c59-a28b-67ca8b33c70b"
+uid = "1293215346651561984"
+async def session(token,proxy,env_name,username):
     custom_headers = {
             "authorization": token,
              "origin": "chrome-extension://lgmpfmgeabnnlemejacfljbmonaomfmm"
@@ -20,22 +20,35 @@ async def toKeep(token,proxy,env_name,username):
     client = TLSClient(account, custom_headers)
     request = await client.post("https://api.nodepay.org/api/auth/session")
     print(request)
-    print(request["code"])
-    if request["code"] == 0:
-        data = {"browser_id": "e1998100-a629-4c59-a28b-67ca8b33c70b","id": request["data"]["uid"], "timestamp":int(time.time()), "version":"2.2.7"}
-        ping = await client.post("https://nw.nodepay.org/api/network/ping",json=data)
-        print(ping)
 
+async def ping(token, browser_id, uid, proxy, env_name, username):
+    custom_headers = {
+        "authorization": token,
+        "origin": "chrome-extension://lgmpfmgeabnnlemejacfljbmonaomfmm"
+    }
+    account = AccountInfo(idx=env_name, proxy=proxy, email_username=username)
+    client = TLSClient(account, custom_headers)
+    data = {"browser_id": browser_id,"id": uid, "timestamp":int(time.time()), "version":"2.2.7"}
+    ping = await client.post("https://nw.nodepay.org/api/network/ping",json=data)
+    print(ping)
 
-
-if __name__ == '__main__':
+def nodepaySessiontask(envName,token):
     with app.app_context():
-        env = Env.query.filter_by(name="Q-0").first()
+        env = Env.query.filter_by(name=envName).first()
         proxy = Proxy.query.filter_by(id=env.t_proxy_id).first()
         account = Account.query.filter_by(id=env.outlook_id).first()
-        print(int(time.time()))
         if proxy:
-            asyncio.run(toKeep(token, proxy.user+":" +proxy.pwd+"@"+proxy.ip+":"+proxy.port, env.name, account.name))
+            asyncio.run(session(token, proxy.user+":" +proxy.pwd+"@"+proxy.ip+":"+proxy.port, env.name, account.name))
         else:
-            asyncio.run(toKeep(token, "", env.name, account.name))
+            asyncio.run(session(token, "", env.name, account.name))
 
+
+def nodepayPingtask(envName,token, browser_id,uid):
+    with app.app_context():
+        env = Env.query.filter_by(name=envName).first()
+        proxy = Proxy.query.filter_by(id=env.t_proxy_id).first()
+        account = Account.query.filter_by(id=env.outlook_id).first()
+        if proxy:
+            asyncio.run(ping(token, browser_id,uid, proxy.user+":" +proxy.pwd+"@"+proxy.ip+":"+proxy.port, env.name, account.name))
+        else:
+            asyncio.run(ping(token, browser_id,uid, "", env.name, account.name))
