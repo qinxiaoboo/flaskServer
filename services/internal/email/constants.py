97ea85fddb79b70edfdde7401ca56a3a1250019c
qinxiaobo <1337556808@qq.com>
2024-10-13 16:47:06 +0800

    邮箱链接api

---
+++ services/internal/email/constants.py
@@ -0,0 +1,12 @@
+IMAP_SERVERS = {
+    'outlook.com': 'outlook.office365.com',
+    'hotmail.com': 'imap-mail.outlook.com',
+    'rambler.ru': 'imap.rambler.ru',
+    'mail.ru': 'imap.mail.ru',
+    'qq.com': 'imap.qq.com',
+}
+
+FOLDERS = ['INBOX', 'JUNK']
+
+MAIL3_SIGN_MESSAGE_FORMAT = "I authorize sending and checking my emails on mail3 from this device. " \
+                            "This doesn't cost anything.\n\nNonce: {{nonce}}"
