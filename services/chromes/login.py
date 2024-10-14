934e0e39a2e173ddc075d204314b6643e35390d9
“Toka” <508808721@qq.com>
2024-10-14 11:03:46 +0800

    OKX钱包登录class更新

--- services/chromes/login.py
+++ services/chromes/login.py
@@ -48,6 +48,7 @@ def LoginPhantomWallet(chrome,env):
         pass
     else:
         chrome.new_tab("chrome-extension://bfnaelmomeimhlpmgjnjophhpkkoljpa/popup.html")
+        chrome.wait(3,5)
         tab = chrome.get_tab(title="Phantom Wallet")
 
     if tab.s_ele("Unlock"):
@@ -62,9 +63,10 @@ def LoginPhantomWallet(chrome,env):
             if wallet:
                 tab.ele("Import an existing wallet").click()
                 tab.ele("@@class=sc-bdvvtL iZUbiK@@text()=Import Secret Recovery Phrase").click()
-                eles = tab.eles("@class=sc-cdJjGe gOBozq")
+                eles = tab.eles("@class=sc-bttaWv gSFlAR")
                 for index, word in enumerate(aesCbcPbkdf2DecryptFromBase64(wallet.word_pass).split(" ")):
                     eles[index].input(word)
+
                 tab.ele("Import Wallet").click()
                 tab.wait(20)
                 tab.ele("Continue").click()
@@ -113,7 +115,7 @@ def LoginOKXWallet(chrome,env):
             wallet = Wallet.query.filter_by(id=env.okx_id).first()
             if wallet:
                 tab.ele("Import wallet").click()
-                tab.ele("@@text()=Import wallet@@style=font-weight: 500; flex: 1 0 0%;").click()
+                tab.ele("@@text()=Seed phrase or private key@@style=font-weight: 500; flex: 1 0 0%;").click()
                 eles = tab.eles("@type=text")
                 for index, word in enumerate(aesCbcPbkdf2DecryptFromBase64(wallet.word_pass).split(" ")):
                     eles[index].input(word)
