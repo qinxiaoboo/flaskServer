db2a5a44b288a9eb7ac2577707ff11883f118198
qinxiaobo <1337556808@qq.com>
2024-12-08 23:41:14 +0800

    pipe项目

--- services/apiTask/signup_pipe.py
+++ services/apiTask/signup_pipe.py
@@ -1,9 +1,23 @@
+from urllib.parse import urlparse, parse_qs
 
 import asyncio
 import json
+import random
 import time
 from flaskServer.services.apiTask.clientApi import TLSClient,userAgent
 import requests
+from flaskServer.mode.account import Account
+from flaskServer.mode.proxy import Proxy
+from flaskServer.config.connect import app,db
+# 代理列表
+proxyList = []
+# 邮箱列表
+accountList=[]
+# 邀请列表
+#第一次执行会生成这个列表
+reffers = ['eXVuMTY2MD', 'Y29jb3NieW', 'a3JvZ2NvdH', 'Y2xhcmtlLm', 'aGVsZW5jbm', 'QW50aG9ueV', 'bWluZGVybW', 'bW9ucm9lZG', 'aGVybmFuZG', 'eXVuMTY2MD']
+# 是否是第一次执行,如果不是第一次执行，则使用上面reffers中的邀请码，并随机选中一个使用
+FIRST=True
 
 
 #注册
@@ -12,47 +26,97 @@ async def signup(env_name, email, datas,proxy):
         "Content-Type": "application/json",
     }
     client = TLSClient(proxy, userAgent, custom_headers)
+    if email =="yun16603860403@outlook.com":
+        password = "5201314xiao"
+    else:
+        password = "123qweasd"
     data = {
         "email": email,
-        "password" : "123qweasd",
-        "referralCode": "",
+        "password" : password,
+        "referralCode": "MTMzNzU1Nj" if FIRST else random.choice(reffers)
     }
-    # response = await client.post("https://api.pipecdn.app/api/signup", json=data, with_text=True)
-    # print(f"{env_name}:{email} {response}")
-    await login(client, env_name, email, datas, proxy)
+    response = await client.post("https://api.pipecdn.app/api/signup", json=data, with_text=True)
+    print(f"{env_name}:{email} {response}")
+    await login(client, env_name, email, datas, proxy, password)
 
 #登录
-async def login(client, env_name, email,datas,proxy):
+async def login(client, env_name, email,datas,proxy,password):
     data = {
         "email": email,
-        "password": "123qweasd",
+        "password": password,
     }
     response = await client.post("https://api.pipecdn.app/api/login", json=data)
     token = response["token"]
-    # await reward(client, email)
+    reffer = await reward(client, email)
+    reffers.append(reffer)
     print(f"环境：{env_name} 登录成功 ，token：{token}")
     datas.append(f"{env_name}|{email}|{token}|{proxy}\n")
+
 # 获取邀请连接
 async def reward(client, email):
     data = {
         "email": email
     }
     response = await client.get("https://api.pipecdn.app/api/generate-referral", json=data)
-    return response["response"]
+    url = response["referralLink"]
+    # 解析 URL
+    parsed_url = urlparse(url)
+    # 获取查询参数部分并解析
+    query_params = parse_qs(parsed_url.query)
+    # 获取 ref 参数的值
+    ref_value = query_params.get('ref', [None])[0]
+    return ref_value
 
 async def main():
+    tasks = []
     datas = []
-    with open(r".\input.txt", "r") as f:
-        data = f.readlines()
-        tasks = []
-        for line in data:
-            env_name, email, token, proxy = line.strip().split("|")
-            await asyncio.sleep(5)
-            task = asyncio.create_task(signup(env_name, email, datas, proxy))
+    for i in range(10):
+        if i == 0:
+            task = asyncio.create_task(signup(i, accountList[i], datas, {}))
             tasks.append(task)
-        await asyncio.gather(*tasks)
+        if i < len(proxyList):
+            task = asyncio.create_task(signup(i+1, accountList[i+1], datas, proxyList[i]))
+            tasks.append(task)
+    await asyncio.gather(*tasks)
     with open(r".\output.txt", "w") as f:
         f.writelines(datas)
 
+def getProxys():
+    with app.app_context():
+        proxys = Proxy.query.all()
+        for proxy in proxys:
+            proxyList.append(f"{proxy.user}:{proxy.pwd}@{proxy.ip}:{proxy.port}")
+    return proxyList
+
+def getDiscordEmailName():
+    with app.app_context():
+        accounts = Account.query.filter(Account.type=="DISCORD").all()
+        for account in accounts:
+            if account.name:
+                accountList.append(account.name)
+
+def getFileEmailName():
+    with open("input.txt") as f:
+        # 按需添加
+        pass
+
+def getTWEmailName():
+    with app.app_context():
+        accounts = Account.query.filter(Account.type=="TW").all()
+        for account in accounts:
+            if account.email_name:
+                accountList.append(account.email_name)
+
+def getOutlookEmailName():
+    #按需添加
+    pass
+
 if __name__ == '__main__':
+
+    getProxys()
+    getDiscordEmailName()
+    # getFileEmailName()
+    print(proxyList)
+    print(accountList)
     asyncio.run(main())
+    print(reffers)
